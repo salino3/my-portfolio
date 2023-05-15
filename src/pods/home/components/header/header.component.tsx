fd263227bc57c3f69abce7f25bdeab222d957819
salino3 <flaviobcn012@gmail.com>
2023-05-15 19:22:50 +0200

    add main images

--- src/pods/home/components/header/header.component.tsx
+++ src/pods/home/components/header/header.component.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Navbar } from '../navbar';
 import * as classes from './header.styles';
 
 export const Header: React.FC = () => {
   return (
-  <header className={classes.root}>
-
-  </header>
+    <header className={classes.root}>
+      <Navbar />
+      <div className={classes.container}>
+        <div className={classes.mainImg}>
+          <img src="/assets/gallery/main-picture.png" alt="main-images" />
+        </div>
+        <div className={classes.boxTitle}>
+          <h1 className={classes.title}>
+            Hi, I am <br />
+            <span>Flavio FORTE</span> <br />
+            FrontEnd Developer
+          </h1>
+        </div>
+      </div>
+    </header>
   );
 }
--- src/pods/home/components/header/header.styles.ts
+++ src/pods/home/components/header/header.styles.ts
@@ -1,8 +1,52 @@
 import {css} from '@emotion/css';
+import { theme } from '../../../../theme';
 
 
 export const root = css`
 
+display: flex;
+flex-direction: column;
+`;
 
+export const container = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const mainImg = css`
+
+img {
+
+    max-width: 170px;
+    width: 100%;
+
+@media (min-width: 1024px) {
+    max-width: 378px;
+  }
+ }
+`;
+
+export const boxTitle = css`
+ 
+ display: flex;
+ flex-direction: column;
+ justify-content: center;
+ text-align: center;
+`;
+
+
+export const title = css`
+  color: ${theme.palette.text_header};
+  font-weight: 700;
+  font-size: 25px;
+  line-height: 37px;
+
+  span {
+    font-size: 34px;
+    line-height: 50px;
+    color: ${theme.palette.danger};
+  }
 `;
 
